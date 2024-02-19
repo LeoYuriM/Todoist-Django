5e139d747427a5faaa14858dc0b8ff7a7aafb1a5
Léo <leonardo.yuri.ms@gmail.com>
2024-02-19 19:00:03 -0300

    First integration Back and Frontend

--- mysite/todoist/views.py
+++ mysite/todoist/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Bloco
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the todoist index.")
+    lista_bloco = Bloco.objects.all()
+    template = loader.get_template("todoist/index.html")
+    context = {
+        "lista_bloco": lista_bloco,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detalhe(request, bloco_id):
     return HttpResponse("Detalhes do Bloco %s." % bloco_id)
