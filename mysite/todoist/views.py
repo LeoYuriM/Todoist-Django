f805d2596256be58ad9b02253de998622d7577c7
Léo <leonardo.yuri.ms@gmail.com>
2024-02-20 17:41:14 -0300

    Correcting directories for the statics, adding DTL in index.html

--- mysite/todoist/views.py
+++ mysite/todoist/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Bloco
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the todoist index.")
+    bloco_list = Bloco.objects.all()
+    template = loader.get_template("todoist/index.html")
+    context = {
+        "bloco_list" : bloco_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def detalhe(request, bloco_id):
     return HttpResponse("Detalhes do Bloco %s." % bloco_id)
