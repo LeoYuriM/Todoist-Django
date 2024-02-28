e60fbbf9d1a0b9ee0a228bed7fa46dcbd8880f4c
Joao Oliveira <joao.vit.maiaoliv@gmail.com>
2024-02-27 21:22:11 -0300

    delete blocks

--- mysite/todoist/static/todoist/js/script.js
+++ mysite/todoist/static/todoist/js/script.js
@@ -1,15 +1,35 @@
 const buttonAddList = document.getElementById('button');
 
+//Clonar formulário
 buttonAddList.onclick = function(){
-        // Impede o comportamento padrão de submissão do formulário no caso recarregar a página
-        event.preventDefault();
+        var originalForm = document.querySelector('.formulario');
+        var clone = originalForm.cloneNode(true);
 
-        // Seleciona o elemento q for ser duplicado
-        var elementoOriginal = document.querySelector(".formulario");
+        // Limpar os campos de texto e área de texto
+        var inputs = clone.querySelectorAll('input[type="text"], textarea');
+        inputs.forEach(function(input) {
+            input.value = '';
+        });
 
-        // Clona o elemento
-        var cloneElemento = elementoOriginal.cloneNode(true);
-    
-        // Insere o clone após o elemento original
-        elementoOriginal.parentNode.insertBefore(cloneElemento, elementoOriginal.nextSibling);
+        originalForm.parentNode.appendChild(clone);
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona o evento de clique ao elemento pai com a classe "block-ul"
+    document.querySelector('.block-ul').addEventListener('click', function(event) {
+        // Verifica se o elemento clicado é um ícone
+        if (event.target.classList.contains('fa-circle-xmark')) {
+            // Encontra o paizão com a classe "formulario" mais próximo
+            var formParent = event.target.closest('.formulario');
+            // Verifica se há mais de um formulário antes de excluir
+            var formCount = document.querySelectorAll('.formulario').length;
+            if (formParent && formCount > 1) {
+                // Se tiver mais de um bloco
+                formParent.remove();
+            }
+            else{
+                alert("Você não pode deixar a página sem blocos de nota!");
+            }
+        }
+    });
+});
