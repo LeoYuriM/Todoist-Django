f9b09718f17f4f4c5ca8c705a5706364f252ef3b
João Vitor Maia <joao.vit.maiaoliv@gmail.com>
2024-03-01 15:34:09 -0300

    change user image

--- mysite/todoist/static/todoist/js/script.js
+++ mysite/todoist/static/todoist/js/script.js
@@ -29,10 +29,12 @@ buttonAddList.onclick = function(){
     // limpa o link de submissão do formulário
     clone.action = "/create";
 
-    // Define a data e hora atual para o input de data e hora do formulário clonado
-    var currentDateTime = new Date().toISOString().slice(0, 16); // Formato: YYYY-MM-DDTHH:mm
+    // Define a data e hora atual para o input de data e hora
+    var currentDateTime = new Date();
+    var currentHour = currentDateTime.getHours().toString().padStart(2, '0');
+    var currentMinute = currentDateTime.getMinutes().toString().padStart(2, '0');
     var dateTimeInput = clone.querySelector('.datetime-local');
-    dateTimeInput.value = currentDateTime.replace('T', ' ');
+    dateTimeInput.value = `${currentDateTime.getFullYear()}-${(currentDateTime.getMonth() + 1).toString().padStart(2, '0')}-${currentDateTime.getDate().toString().padStart(2, '0')}T${currentHour}:${currentMinute}`;
 
     // Adiciona o formulário clonado ao final da lista de formulários
     originalForm.parentNode.appendChild(clone);
@@ -45,3 +47,31 @@ buttonAddList.onclick = function(){
         clone.classList.remove('hide');
     }, 50); // Tempo em milissegundos
 }
+
+// mudar foto de usuário
+document.addEventListener("DOMContentLoaded", function() {
+    var userPhoto = document.getElementById("userPhoto");
+    var fileInput = document.getElementById("fileInput");
+
+    // Adiciona um ouvinte de evento de clique à foto do usuário
+    userPhoto.addEventListener("click", function() {
+        // Simula um clique no input de arquivo quando a foto do usuário é clicada
+        fileInput.click();
+    });
+
+    // Adiciona um ouvinte de evento de alteração ao input de arquivo
+    fileInput.addEventListener("change", function() {
+        // Verifica se um arquivo foi selecionado
+        if (fileInput.files && fileInput.files[0]) {
+            var reader = new FileReader();
+            
+            // Quando o arquivo é carregado, atualiza a src da imagem com o arquivo selecionado
+            reader.onload = function(e) {
+                userPhoto.src = e.target.result;
+            }
+            
+            // Lê o arquivo como uma URL de dados
+            reader.readAsDataURL(fileInput.files[0]);
+        }
+    });
+});
